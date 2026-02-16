bf141182124013298e58dbe3be9af11eeaf3d9db
yevhents-homealliance <yevhen.ts@homealliance.com>
2026-02-16 20:11:16 +0200

    f

--- script/ai.js
+++ script/ai.js
@@ -1,4 +1,4 @@
-/*async function askAI(startMessage) {
+async function askAI(startMessage) {
 
     const sendBtn = document.getElementById('sendBtn');
     sendBtn.classList.add('loading');
@@ -55,87 +55,6 @@
         setTimeout(() => {
             aiMessage.classList.add('chat__message--visible');
         }, 10);
-
-    } catch (err) {
-        console.log(err);
-    } finally {
-        sendBtn.classList.remove('loading');
-        sendBtn.disabled = false;
-    }
-}
-*/
-
-let chatHistory = [];
-
-async function askAI(startMessage) {
-
-    const sendBtn = document.getElementById('sendBtn');
-    sendBtn.classList.add('loading');
-    sendBtn.disabled = true;
-
-    const welcome__block = document.querySelector('.chat__welcome');
-    const message = startMessage ? startMessage : document.getElementById('chatInput').value;
-    const chatInput = document.querySelector('.chat__input');
-    chatInput.style.bottom = '0px';
-
-    welcome__block.style.display = 'none';
-
-    if (!message) {
-        sendBtn.classList.remove('loading');
-        sendBtn.disabled = false;
-        return;
-    }
-
-    const container = document.querySelector('.chat__messages');
-    const message__user = document.createElement('div');
-
-    message__user.classList.add('chat__user-message', 'chat__message');
-    message__user.textContent = message;
-
-    container.appendChild(message__user);
-
-    setTimeout(() => {
-        message__user.classList.add('chat__message--visible');
-    }, 10);
-
-    document.getElementById('chatInput').value = '';
-
-    chatHistory.push({
-        role: "user",
-        parts: [{ text: message }]
-    });
-
-    try {
-        const res = await fetch('https://c86b-176-98-30-40.ngrok-free.app/api/ask-faq', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ contents: chatHistory })
-        });
-
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-
-        const data = await res.json();
-
-        const aiText = data.answer || 'Упс, щось пішло не так. Спробуйте ще раз.';
-
-        const aiMessage = document.createElement('div');
-        aiMessage.classList.add('chat__ai-message', 'chat__message');
-        aiMessage.textContent = aiText;
-
-        container.appendChild(aiMessage);
-
-        setTimeout(() => {
-            aiMessage.classList.add('chat__message--visible');
-        }, 10);
-
-        chatHistory.push({
-            role: "model",
-            parts: [{ text: aiText }]
-        });
 
     } catch (err) {
         console.log(err);
